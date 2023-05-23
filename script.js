17783be5b2b8d03442581caf1540ddb4e838bb36
Pavel-Kapichnikov <131277581+Pavel-Kapichnikov@users.noreply.github.com>
2023-05-23 14:46:43 +0300

    Commit 23/05/2023

--- script.js
+++ script.js
@@ -1,39 +1,7 @@
 let swiper;
 
-swiper = new Swiper('.swiper', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-
-    slidesPerView: 'auto',
-
-    slidesOffsetBefore: 16,
-
-    spaceBetween: 16,
-
-    slidesOffsetAfter: 16,
-
-    init: false,
-});
-
-if (window.innerWidth < 768) swiper.init();
-
-const mediaQuery = '(min-width: 768px)';
-const mediaQueryList = window.matchMedia(mediaQuery);
-
-mediaQueryList.addEventListener('change', event => {
-    console.log(window.innerWidth);
-
-  if (event.matches) {
-    console.log('ipad');
-    swiper.destroy();
-
-  } else {
-    console.log('smartphone');
-
+function initSwiper() {
     swiper = new Swiper('.swiper', {
-
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
@@ -47,20 +15,33 @@ mediaQueryList.addEventListener('change', event => {
     
         slidesOffsetAfter: 16,
     });
-  }
-})
+}
+
+if (matchMedia) {
+    let screen = window.matchMedia('(max-width: 767px)');
+    screen.addListener(changes);
+    changes(screen);
+}
+
+function changes (screen) {
+    if (screen.matches) {
+        initSwiper();
+    } else if (swiper) {
+        swiper.destroy();
+    }
+}
 
 function readMore() {
             
-    let btn = document.querySelector('.readMore');
+    let btn = document.getElementById('readMore');
     let cardsList = document.querySelector('.supported-brands__list');
 
     if (btn.textContent === 'Показать все') {
-        cardsList.style = 'height: auto; margin-bottom: 46px;';
+        cardsList.classList.add('supported-brands__list--activ');
         btn.classList.add('readMore--activ');
         btn.textContent = "Скрыть";
     } else {
-        cardsList.style = 'height: 160px; margin-bottom: 24px;';
+        cardsList.classList.remove('supported-brands__list--activ');
         btn.classList.remove('readMore--activ');
         btn.textContent = "Показать все";
     }
